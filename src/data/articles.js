ebe859d91999f9a395489ea209ffa0af6c14aac7
Naggayi daphne Perl <daphnepearl01@gmail.com>
2023-12-24 10:29:08 +0300

    seo page

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,9 +8,9 @@ function article_1() {
 			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
 		keywords: [
 			"The Benefits of Cloud Computing",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
+			"Malik Akorede",
+			"Malike Akorede",
+			"Malik Akorede",
 		],
 		style: `
 				.article-content {
@@ -48,9 +48,9 @@ function article_2() {
 		style: ``,
 		keywords: [
 			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
+			"Malik Akorede",
+			"Malike Akorede",
+			"Malike Akorede",
 		],
 		body: (
 			<React.Fragment>
