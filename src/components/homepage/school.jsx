1586044103bee63e49e213d783a95eb707827806
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-05-31 13:57:38 +0300

    add schools

--- src/components/homepage/school.jsx
+++ src/components/homepage/school.jsx
@@ -8,66 +8,56 @@ import "./styles/article.css";
 const Schools = () => {
 	return (
 		<div className="homepage-article">
-			<Card
-				icon={faSchool}
-				title="Education"
-				body={
-					<div className="works-body">
-						<div className="work">
-							<img
-								src="../refactory.jpeg"
-								alt="refactory"
-								className="work-image"
-							/>
-							<div className="homepage-article-title">
-								Certificate in Software Engineer
-							</div>
-							<div className="work-subtitle">Refactory</div>
-							{/* <div className="work-duration">2022 - 2023</div> */}
-						</div>
+			<div className="works-body">
+				<div className="work">
+					<img
+						src="../refactory.jpeg"
+						alt="refactory"
+						className="work-image"
+					/>
+					<div className="homepage-article-title">
+						Certificate in Software Engineer
+					</div>
+					<div className="work-subtitle">Refactory</div>
+					{/* <div className="work-duration">2022 - 2023</div> */}
+				</div>
 
-						<div className="work">
-							<img
-								src="../makerere-logo.jpeg"
-								alt="makerere"
-								className="work-image"
-							/>
-							<div className="homepage-article-title">
-								Cisco Certified Networking Associate CCNA
-							</div>
-							<div className="work-subtitle">
-								Makerere University
-							</div>
-						</div>
+				<div className="work">
+					<img
+						src="../makerere-logo.jpeg"
+						alt="makerere"
+						className="work-image"
+					/>
+					<div className="homepage-article-title">
+						Cisco Certified Networking Associate CCNA
+					</div>
+					<div className="work-subtitle">Makerere University</div>
+				</div>
 
-						<div className="work">
-							<img
-								src="../makerere-logo.jpeg"
-								alt="refactory"
-								className="work-image"
-							/>
-							<div className="homepage-article-title">
-								Bachelor in Science in Computer Science
-							</div>
-							<div className="work-subtitle">
-								Makerere University
-							</div>
-						</div>
+				<div className="work">
+					<img
+						src="../makerere-logo.jpeg"
+						alt="refactory"
+						className="work-image"
+					/>
+					<div className="homepage-article-title">
+						Bachelor in Science in Computer Science
+					</div>
+					<div className="work-subtitle">Makerere University</div>
+				</div>
 
-						<div className="work">
-							{/* <img
+				<div className="work">
+					{/* <img
 								src="../makerere-logo.jpeg"
 								alt="refactory"
 								className="work-image"
 							/> */}
-							<div className="homepage-article-title">
-								Certificate in Data Protection Training{" "}
-							</div>
-							<div className="work-subtitle">Tecno Legal </div>
-						</div>
+					<div className="homepage-article-title">
+						Certificate in Data Protection Training{" "}
 					</div>
-				}
-			/>
+					<div className="work-subtitle">Tecno Legal </div>
+				</div>
+			</div>
 		</div>
 	);
 };
